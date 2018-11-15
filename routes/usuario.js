b76937014e1eaa3da819cf113d802c8599913c70
Alan Islas <alan.islas.itic@gmail.com>
2018-11-15 11:37:14 -0600

    Terminando la peticion PUT para actualizar usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,6 +1,8 @@
 var express = require('express');
 
 var Usuario = require('../models/usuario');
+var bcrypt = require('bcrypt');
+
 
 var app = express();
 
@@ -29,6 +31,55 @@ app.get('/', (req, res, next) => {
 });
 
 // ==================================================
+// Acualizar usuario
+// ==================================================
+
+app.put('/:id', (req, res) => {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Usuario.findById(id, 'nombre email img role')
+        .exec((err, usuario) => {
+
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar usuario',
+                    errors: err
+                });
+            }
+            if (!usuario) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El usuario con el id' + id + ' no existe',
+                    errors: err
+                });
+            }
+            usuario.nombre = body.nombre;
+            usuario.email = body.email;
+            usuario.role = body.role;
+
+            usuario.save((err, usuarioActualizado) => {
+
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Error al actualizar el usuario en base de datos',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioActualizado
+                });
+
+            });
+        });
+});
+
+// ==================================================
 // Crear un nuevo usuario
 // ==================================================
 app.post('/', (req, res) => {
@@ -37,7 +88,7 @@ app.post('/', (req, res) => {
     var usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: body.password,
+        password: bcrypt.hashSync(body.password, 10),
         img: body.img,
         role: body.role
     });
