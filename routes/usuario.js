026207cc07610decb6ffbed06b06fd1d4e57d23d
Alan Islas <alan.islas.itic@gmail.com>
2018-11-14 22:27:19 -0600

    Haciendo primer petición post para crear un usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,12 +1,61 @@
 var express = require('express');
 
+var Usuario = require('../models/usuario');
+
 var app = express();
 
+// ==================================================
+// Obtener todos los usuarios
+// ==================================================
+
 app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Get de usuarios'
+
+    Usuario.find({}, 'nombre email img role')
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error cargado usuarios',
+                    errors: err
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                mensaje: usuarios
+            });
+        });
+
+
+});
+
+// ==================================================
+// Crear un nuevo usuario
+// ==================================================
+app.post('/', (req, res) => {
+    var body = req.body;
+
+    var usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        img: body.img,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioGuardado) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al guardar el usuario en base de datos',
+                errors: err
+            });
+        }
+        res.status(201).json({
+            ok: true,
+            usuario: usuarioGuardado
+        });
     });
 });
+
 
 module.exports = app;
