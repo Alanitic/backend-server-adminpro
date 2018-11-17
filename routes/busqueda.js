4ceef75ac1bf60c4ca8d96fac3b15cdcf27aa745
Alan Islas <alan.islas.itic@gmail.com>
2018-11-17 17:45:31 -0600

    Utilizando el Promise.all() para manejar búsquedas simultaneas

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -13,15 +13,20 @@ app.get('/todo/:busqueda', (req, res, next) => {
     // La i es de que no considere mayúsculas
     var regex = new RegExp(busqueda, 'i');
 
-    buscarHospitales(busqueda, regex)
-        .then(hospitales => {
-
+    Promise.all(
+            [buscarHospitales(busqueda, regex), buscarMedicos(busqueda, regex)])
+        .then(respuestas => {
             res.status(200).json({
                 ok: true,
-                hospitales: hospitales
+                hospitales: respuestas[0],
+                medicos: respuestas[1]
             });
         });
 });
+
+// ==================================================
+// Promesa para buscar Hoteles
+// ==================================================
 
 function buscarHospitales(busqueda, regexp) {
 
@@ -32,6 +37,26 @@ function buscarHospitales(busqueda, regexp) {
                 reject('Error al cargar hospitales', err);
             } else {
                 resolve(hospitales);
+            }
+        });
+    });
+
+}
+
+
+// ==================================================
+// Promesa para buscar Medicos
+// ==================================================
+
+function buscarMedicos(busqueda, regexp) {
+
+    return new Promise((resolve, reject) => {
+
+        Medico.find({ nombre: regexp }, (err, medicos) => {
+            if (err) {
+                reject('Error al cargar hospitales', err);
+            } else {
+                resolve(medicos);
             }
         });
     });
