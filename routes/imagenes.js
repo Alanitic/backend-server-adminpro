15873ccdde61eb1488dd6a52b5c961f194542ce9
Alan Islas <alan.islas.itic@gmail.com>
2018-11-18 11:43:40 -0600

    Ruta para obtener imagen terminada

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
+const path = require('path');
+const fs = require('fs');
+
+app.get('/:tipo/:img', (req, res, next) => {
+
+    var tipo = req.params.tipo;
+    var img = req.params.img;
+
+    var pathImagen = path.resolve(__dirname, `../uploads/${tipo}/${img}`);
+
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    } else {
+        var pathNoImage = path.resolve(__dirname, '../assets/no-img.jpg');
+        res.sendFile(pathNoImage)
+    }
 });
 
 module.exports = app;
